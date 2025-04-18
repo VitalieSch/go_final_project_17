204d55e77f82c9c24d5b33d02fda00bc90b72e59
Vitalie Schlat <vschlat@gmail.com>
2025-04-18 11:36:07 +0300

    second commit

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"database/sql"
+	"go1f/pkg/database"
 	"go1f/pkg/server"
+	"log"
 )
 
 func main() {
+	var db *sql.DB
+
+	err := database.Init("scheduler.db")
+	if err != nil {
+		log.Fatalf("не удалось получить БД: %v\n", err)
+	}
+
+	defer db.Close()
 
 	server.Run()
 
